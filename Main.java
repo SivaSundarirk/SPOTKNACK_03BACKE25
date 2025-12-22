ea4c36ccb94c694f65740b2759d14576c0c00703
Siva Sundari R K <sivasundarirk@gmail.com>
2025-12-22 19:09:39 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,51 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-class BankAccount
+import java.util.Scanner;
+class Main
 {
-    private String AccountNumber;
-    private double balance;
-     public BankAccount(String AccountNumber,double initaldeposit)
+    public static void main(String[] agrs)
+    /**
+     * To print the even number
+     * until we print the even the loop continusoly get thr number from the user
+     * @param args
+     */
     {
-        AccountNumber=AccountNumber;
-        balance=initaldeposit;
-    }
-     public BankAccount(String AccountNumber)
-    {
-        AccountNumber=AccountNumber;
-        balance=0.0;
-    }
-    public void deposit(double amount) {
-        if (amount > 0) {
-            balance = balance + amount;
-            System.out.println("Deposited :" + amount);
-        } else {
-            System.out.println("Invaild amount please try again");
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter a number");
+        int number=scan.nextInt();
+        if(number%2==0)
+        {
+            System.out.println("This is  a even number");
+        }
+        else
+        {
+            for(int i=0;i<50;i++)
+            {
+                Scanner scan2=new Scanner(System.in);
+                System.out.print("Enter a number");
+                int number2=scan2.nextInt();
+                if(number2%2==0)
+                {
+                    System.out.println("This is even number");
+                    break;
+                }
+            }
         }
     }
-    public Void withdraw(double amount){
-
-        if (amount > 0 && amount <= balance) {
-            balance = balance - amount;
-            System.out.println("Withdrawn :" + amount);
-        } else {
-            System.out.println("No sufficent balance in you Account");
-        }
-    }
-    public void display(){
-
-    System.out.println("Account number:"+AccountNumber);
-    System.out.println("Balance:"+balance);
-}
-    public static void main (String[] args) {
-    BankAccount acc1 = new BankAccount("ACC001", 10000.0);
-    acc1.deposit(5000);
-    acc1.withdraw(7000);
-    acc1.display();
-    System.out.println();
-    BankAccount acc2 = new BankAccount("ACC002");
-    acc2.deposit(2000);
-    acc2.withdraw(500);
-    acc2.display();
-}
-
 }
