6c93b15901252b4e5a63807f7940bfa7b7062b37
Siva Sundari R K <sivasundarirk@gmail.com>
2026-02-27 00:56:34 +0530

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,33 @@
+package com.spk.patientrecordmanagemet.model;
+
+public class Admin {
+    int adminid;
+    String adminname;
+    String password;
+    Admin(int adminid, String adminname,String password)
+    {
+        this.adminid=adminid;
+        this.adminname=adminname;
+        this.password=password;
+    }
+}
+public void addDoctor()
+{
+
+}
+public void removeDoctor()
+{
+
+}
+public void addpatient()
+{
+
+}
+public void viewDashboard()
+{
+
+}
+public void generateReport()
+{
+
+}
